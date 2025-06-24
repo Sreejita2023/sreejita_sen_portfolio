9b2ed0e27bae1e2b87d25643f27f2bd1f339b4c2
Sreejita2023 <sensreejita67@gmail.com>
2025-06-25 00:24:00 +0530

    project updated

--- src/utils/data/projectsData.js
+++ src/utils/data/projectsData.js
@@ -1,9 +1,20 @@
 import shopeasy from '../../assests/projects/shopeasy.png'
-import blessedBasket from "../../assests/projects/blessedbaskets.png"
-import crytography from "../../assests/projects/crytography.png"
+import blessedBasket from "../../assests/projects/blessedbaskets.webp"
+import crytography from "../../assests/projects/crytography.webp"
+import gamemate from "../../assests/projects/gamemate.webp"
 export const projectsData =[
     {
         id: 1,
+        name: 'Gamemate',
+        description: "Fostered a vibrant gaming community by enabling players to connect with friends, challenge them to 4 solo and dual games, and compare scores . Enhanced the overall user experience by adding 2 solo game options, catering to a wider audience.",
+        tools: ['React Js', 'Node Js','Javascript' ,'MongoDB Compass','MongoDb Atlas' ,'Postman' ,'Express Js'],
+        role: 'Frontend',
+        code: 'https://github.com/Sreejita2023/CollegeProject',
+        demo: 'https://wonderful-water-0f9d54400.5.azurestaticapps.net/',
+        image: gamemate,
+    },
+    {
+        id: 2,
         name: 'Blessed Baskets',
         description: "Led a team of 3 as a Frontend Developer and designed a platform to combat the global food waste crisis, addressing the 1.3 billion tons of food waste by connecting donors with surplus food to those in need.Developed an intuitive interface that implements create, read, update and delete operations.",
         tools: ['React Js', 'Node Js','Javascript' ,'MongoDB Compass','MongoDb Atlas' ,'Postman' ,'Express Js'],
@@ -13,7 +24,7 @@ export const projectsData =[
         image: blessedBasket,
     },
     {
-        id: 2,
+        id: 3,
         name: 'Cryptologic Security',
         description: "Utilized Armstrong Cryptography with Bit Manipulation focusing on bit-level encryption for secure data handling and reduced average computation time by 25 % Innovatively Integrated Color Cryptography to fortify data security.",
         tools: ['C++','Crytography' ,'Git' ,'VS Code'],
@@ -23,7 +34,7 @@ export const projectsData =[
         image: crytography,
     },
     {
-        id: 3,
+        id: 4,
         name: 'Shopeasy Project',
         description: "Utilized Redux with React.js for state management, integrated Tailwind CSS for web page design, and        implemented data fetching from JSON files.Implemented Firebase for email authentication.",
         tools: ['Firebase', 'Html', 'ReactJs','Tailwind Css', 'Cloudinary', 'Redux ','Authentication'],
